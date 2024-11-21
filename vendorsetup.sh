a5c5d8918c7df66d3fd399cc46beaa90a4d31f30
Liwhy <100075328+liwhy1@users.noreply.github.com>
2024-11-21 19:03:41 +0100

    denniz: Apply common patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 git clone https://gitlab.com/nattolecats/android_vendor_oneplus_denniz-firmware vendor/oplus/denniz-firmware --depth 1
+
+bash device/oplus/mt6893-common/patches/apply.sh
