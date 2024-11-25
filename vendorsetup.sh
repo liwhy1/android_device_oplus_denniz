30ddb58285470a37ca9a9757c61d5892aa634f98
Liwhy <100075328+liwhy1@users.noreply.github.com>
2024-11-25 20:18:02 +0100

    denniz: Merge lineage tree changes

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone firmware
+git clone https://gitlab.com/nattolecats/android_vendor_oneplus_denniz-firmware vendor/oplus/denniz-firmware --depth 1
+
+# Make sure KernelSU is registered as a submodule
+cd kernel/oplus/mt6893 && git submodule update --init && cd -
+
+# Apply patches
+bash device/oplus/denniz/patches/apply.sh
