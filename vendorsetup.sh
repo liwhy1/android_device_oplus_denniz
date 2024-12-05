cdd1a1fd0d83afa5550d9dc0b151fa3d34555f6d
Liwhy <100075328+liwhy1@users.noreply.github.com>
2024-12-05 22:39:15 +0100

    denniz: Move firmware to local_manifests

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Clone firmware
-git clone https://gitlab.com/nattolecats/android_vendor_oneplus_denniz-firmware vendor/oplus/denniz-firmware --depth 1
-
 # Make sure KernelSU is registered as a submodule
 cd kernel/oplus/mt6893 && git submodule update --init && cd -
 
