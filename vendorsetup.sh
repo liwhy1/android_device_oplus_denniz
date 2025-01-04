e28a8acef561dd6533497ae0d4911ae1753ad6d8
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-05 00:42:21 +0100

    denniz: Drop 2ND_ARCH_VARIANT patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,3 @@ cd kernel/oplus/mt6893 && git submodule update --init && cd -
 
 # Apply patches
 bash device/oplus/denniz/patches/apply.sh
-
-# Dirty Patch
-# Update BoardConfig 2ND_ARCH_VARIANT
-sed -i "s/"armv8-a"/"armv8-2a"/g" "device/oplus/mt6893-common/BoardConfigCommon.mk"
