ceeb440ace9ec5d1191452ab7c834e046474b1e2
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-22 13:17:04 +0100

    denniz: Make sure we return to build home

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,4 +3,4 @@
 # Make sure KernelSU is registered as a submodule
 cd kernel/oplus/mt6893 && git submodule update --init && cd -
 
-cd device/oplus/camera/patches && bash apply.sh
+cd device/oplus/camera/patches && bash apply.sh && cd -
