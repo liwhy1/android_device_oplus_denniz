c289a5da9773a7cdc867b31010bdee7cf55f27cb
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-23 00:36:30 +0100

    denniz: Run patches properly

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run Patches
-bash device/oplus/camera/patches/apply.sh
+cd device/oplus/camera/patches && apply.sh && cd -
 
 # Make sure KernelSU is registered as a submodule
 cd kernel/oplus/mt6893 && git submodule update --init && cd -
