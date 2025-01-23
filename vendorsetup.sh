0d8bc05ac6126aa78567a44eb8b96c5f1601e87b
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-23 13:46:34 +0100

    denniz: Move patches to dt

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Run Patches
-cd device/oplus/camera/patches && apply.sh && cd -
-
 # Make sure KernelSU is registered as a submodule
 cd kernel/oplus/mt6893 && git submodule update --init && cd -
+
+# Source patches
+bash device/oplus/denniz/patches/apply.sh
