56a0a7bf47ff8b26f453a96bd56106f6bc946eaa
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-31 16:59:10 +0100

    Revert "denniz: vendorsetup: Add dirty udfps patch"
    This reverts commit c89d011a16680ef9cc8da4829a6f0bd23e5f7b85.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,3 @@
 
 # Make sure KernelSU is registered as a submodule
 cd kernel/oplus/mt6893 && git submodule update --init && cd -
-
-# Dirty Patch
-# Update UdfpsDimZOrder
-sed -i "s/"0x41000005"/"0x41000033"/g" "hardware/oplus/hidl/fingerprint/UdfpsExtension.cpp"
