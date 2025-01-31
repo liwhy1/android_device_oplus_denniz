7ac6a7f1dc87c1833472637ab0c3d1597dfbe274
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-31 17:47:45 +0100

    [tmp] denniz: Source conflict fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,7 @@
 
 # Make sure KernelSU is registered as a submodule
 cd kernel/oplus/mt6893 && git submodule update --init && cd -
+
+rm -rf frameworks/base/core/java/com/oplus/util/OplusTypeCastingHelper.java
+rm -rf frameworks/base_new/core/java/com/oplus/util/OplusTypeCastingHelper.java
+rm -rf frameworks/base/new/core/java/com/oplus/util/OplusTypeCastingHelper.java
