9442a017712c2f989f3a46d69f4adb62220faba0
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-02-09 17:09:49 +0100

    denniz: Simplify KSU NEXT initialization

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Make sure KernelSU is registered as a submodule
-cd kernel/oplus/mt6893 && rm -rf KernelSU-Next && git clone https://github.com/Kingslayer9988/KernelSU-Next -b next KernelSU-Next cd -
+rm -rf kernel/oplus/mt6893/KernelSU-Next && git clone https://github.com/Kingslayer9988/KernelSU-Next -b next kernel/oplus/mt6893/KernelSU-Next
