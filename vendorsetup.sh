7017a72b9349e06ba0adb0c4ca9620cdd719a70b
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-02-09 16:59:26 +0100

    denniz: Clone KSU Next

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Make sure KernelSU is registered as a submodule
-cd kernel/oplus/mt6893 && git submodule update --init && cd -
+cd kernel/oplus/mt6893 && rm -rf KernelSU-Next && git clone https://github.com/Kingslayer9988/KernelSU-Next -b next KernelSU-Next cd -
