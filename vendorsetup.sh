578250378a57945d70ca78ac829f5ce377fd70f2
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-02-10 10:17:27 +0100

    denniz: Move to different ksu next blob

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Make sure KernelSU is registered as a submodule
-rm -rf kernel/oplus/mt6893/KernelSU-Next && git clone https://github.com/Kingslayer9988/KernelSU-Next -b next kernel/oplus/mt6893/KernelSU-Next
+rm -rf kernel/oplus/mt6893/KernelSU-Next && git clone https://github.com/soulspark666/KernelSU-Next/  -b next kernel/oplus/mt6893/KernelSU-Next
