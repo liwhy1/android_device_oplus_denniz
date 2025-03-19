fce507aa73c3dd087560004e4de604263dd60481
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-20 00:40:29 +0100

    Revert "denniz: Add patch for fingerprint in vendorsetup"
    This reverts commit 9adcf53e0c0d741b60416da617edc1741decc3ab.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,3 @@
 
 # Make sure KernelSU is registered as a submodule
 cd kernel/oplus/mt6893 && git submodule update --init && cd -
-
-# Modify hw/oplus fingerprint zorder manually
-sed -i 's/return 0x41000005;/return 0x41000033;/' hardware/oplus/fingerprint/UdfpsExtension.cpp
