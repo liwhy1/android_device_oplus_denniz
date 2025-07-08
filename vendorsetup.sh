72b8b40bef8af482d6a7d422978e1c73645eb147
liwhy1 <levi.toth2005@gmail.com>
2025-07-08 15:04:04 +0200

    denniz: Clone clang 17.0.2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,6 @@
 
 # Make sure KernelSU is registered as a submodule
 cd kernel/oplus/mt6893 && git submodule update --init && cd -
+
+# Clone clang r487747c
+git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r487747c prebuilts/clang/host/linux-x86/clang-r487747c --depth 1
